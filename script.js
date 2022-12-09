99d8f8b32074c33fd8c18ac043e5d5357bc61c03
omassey1 <65238159+omassey1@users.noreply.github.com>
2022-12-09 10:42:11 -0500

    val

--- script.js
+++ script.js
@@ -53,5 +53,8 @@ $(document).ready(function () {
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
     // TODO: Add code to display the current date in the header of the page.
    
